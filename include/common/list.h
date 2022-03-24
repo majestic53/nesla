d398397c090eb1a4285ac63439baeef32b520d9b
David Jolly <majestic53@gmail.com>
2022-03-24 15:02:36 -0700

    add list type

---
+++ include/common/list.h
@@ -0,0 +1,114 @@
+/*
+ * NESLA
+ * Copyright (C) 2022 David Jolly
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+ * @file list.h
+ * @brief Linked list.
+ */
+
+#ifndef NESLA_LIST_H_
+#define NESLA_LIST_H_
+
+#include <error.h>
+
+/*!
+ * @struct nesla_list_entry_s
+ * @brief List entry context.
+ */
+typedef struct nesla_list_entry_s {
+    struct nesla_list_entry_s *next;        /*!< Next entry */
+    struct nesla_list_entry_s *previous;    /*!< Previous entry */
+    const void *context;                    /*!< Entry context */
+} nesla_list_entry_t;
+
+/*!
+ * @struct nesla_list_t
+ * @brief List context.
+ */
+typedef struct {
+    nesla_list_entry_t *head;               /*!< Head entry */
+    nesla_list_entry_t *tail;               /*!< Tail entry */
+    size_t size;                            /*!< Entries */
+} nesla_list_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/*!
+ * @brief Get list entry context at index.
+ * @param[in] list Pointer to list context
+ * @param[in] index Entry index
+ * @param[in,out] entry Pointer to list entry context, or NULL
+ * @return NESLA_ERROR on failure, NESLA_SUCCESS otherwise
+ */
+nesla_error_e nesla_list_get(const nesla_list_t *list, size_t index, nesla_list_entry_t *entry);
+
+/*!
+ * @brief Get list entry context at head.
+ * @param[in] list Pointer to list context
+ * @return Pointer to list entry head context, or NULL
+ */
+nesla_list_entry_t *nesla_list_get_head(const nesla_list_t *list);
+
+/*!
+ * @brief Get list size.
+ * @param[in] list Pointer to list context
+ * @return List size
+ */
+size_t nesla_list_get_size(const nesla_list_t *list);
+
+/*!
+ * @brief Get list entry context at tail.
+ * @param[in] list Pointer to list context
+ * @return Pointer to list entry tail context, or NULL
+ */
+nesla_list_entry_t *nesla_list_get_tail(const nesla_list_t *list);
+
+/*!
+ * @brief Insert context into list context after list entry context.
+ * @param[in,out] list Pointer to list context
+ * @param[in,out] entry Pointer to list entry context to insert after, or NULL
+ * @param[in] context Constant pointer to context
+ * @return NESLA_ERROR on failure, NESLA_SUCCESS otherwise
+ */
+nesla_error_e nesla_list_insert(nesla_list_t *list, nesla_list_entry_t *entry, const void *context);
+
+/*!
+ * @brief Remove list entry context from list context.
+ * @param[in,out] list Pointer to list context
+ * @param[in,out] entry Pointer to list entry context
+ * @return NESLA_ERROR on failure, NESLA_SUCCESS otherwise
+ */
+nesla_error_e nesla_list_remove(nesla_list_t *list, nesla_list_entry_t *entry);
+
+/*!
+ * @brief Reset list context.
+ * @param[in,out] list Pointer to list context
+ * @return NESLA_ERROR on failure, NESLA_SUCCESS otherwise
+ */
+nesla_error_e nesla_list_reset(nesla_list_t *list);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* NESLA_LIST_H_ */
---
+++ src/common/list.c
@@ -0,0 +1,167 @@
+/*
+ * NESLA
+ * Copyright (C) 2022 David Jolly
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*!
+ * @file list.c
+ * @brief Linked list.
+ */
+
+#include <common.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+nesla_error_e nesla_list_get(const nesla_list_t *list, size_t index, nesla_list_entry_t *entry)
+{
+    nesla_error_e result = NESLA_SUCCESS;
+
+    if(index >= list->size) {
+        result = SET_ERROR("Invalid index: %zu", index);
+        goto exit;
+    }
+
+    entry = list->head;
+
+    for(size_t offset = 0; offset < index; ++offset) {
+        entry = entry->next;
+    }
+
+exit:
+    return result;
+}
+
+nesla_list_entry_t *nesla_list_get_head(const nesla_list_t *list)
+{
+    return list->head;
+}
+
+size_t nesla_list_get_size(const nesla_list_t *list)
+{
+    return list->size;
+}
+
+nesla_list_entry_t *nesla_list_get_tail(const nesla_list_t *list)
+{
+    return list->tail;
+}
+
+nesla_error_e nesla_list_insert(nesla_list_t *list, nesla_list_entry_t *entry, const void *context)
+{
+    nesla_list_entry_t *new_entry;
+    nesla_error_e result = NESLA_SUCCESS;
+
+    if(!(new_entry = calloc(1, sizeof(*new_entry)))) {
+        result = SET_ERROR("Failed to allocate list entry: %s", strerror(errno));
+        goto exit;
+    }
+
+    new_entry->context = context;
+
+    if(!entry) {
+
+        if(list->head) {
+            new_entry->next = list->head;
+            list->head->previous = new_entry;
+        }
+    } else if(entry == list->tail) {
+        new_entry->previous = entry;
+        entry->next = new_entry;
+    } else {
+        new_entry->next = entry->next;
+        new_entry->previous = entry;
+        entry->next = new_entry;
+    }
+
+    if(!new_entry->previous) {
+        list->head = new_entry;
+    }
+
+    if(!new_entry->next) {
+        list->tail = new_entry;
+    }
+
+    ++list->size;
+
+exit:
+
+    if((result == NESLA_FAILURE) && new_entry) {
+        free(new_entry);
+    }
+
+    return result;
+}
+
+nesla_error_e nesla_list_remove(nesla_list_t *list, nesla_list_entry_t *entry)
+{
+    nesla_error_e result = NESLA_SUCCESS;
+
+    if(!list->size) {
+        result = SET_ERROR("Empty list: %zu", list->size);
+        goto exit;
+    }
+
+    if(entry == list->head) {
+
+        if(entry->next) {
+            entry->next->previous = NULL;
+        }
+
+        list->head = entry->next;
+    } else if(entry == list->tail) {
+
+        if(entry->previous) {
+            entry->previous->next = NULL;
+        }
+
+        list->tail = entry->previous;
+    } else {
+        entry->previous->next = entry->next;
+        entry->next->previous = entry->previous;
+    }
+
+    free(entry);
+    --list->size;
+
+exit:
+    return result;
+}
+
+nesla_error_e nesla_list_reset(nesla_list_t *list)
+{
+    nesla_error_e result = NESLA_SUCCESS;
+
+    while(nesla_list_get_size(list)) {
+
+        if((result = nesla_list_remove(list, nesla_list_get_head(list))) == NESLA_FAILURE) {
+            goto exit;
+        }
+    }
+
+    memset(list, 0, sizeof(*list));
+
+exit:
+    return result;
+}
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
